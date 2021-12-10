6d691d1521351a026e79a5676fb0509f92361f30
matheusasimoes <91484895+matheusasimoes@users.noreply.github.com>
2021-12-10 14:43:05 -0300

    sistema

---
+++ sistema.js
@@ -0,0 +1,48 @@
+let numeroDeAlunos = 10;
+
+// for 
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    //console.log(contador)
+    if (contador == 0) {
+        console.log("O número atual é zero")
+    } else if (contador % 2 == 0) {
+        //usando concatenação
+        console.log("O número " + contador + " é Par")
+    } else {
+        //usando irterpolação
+        console.log(`O número ${contador} é Ímpar`)
+    }
+}
+
+//another for
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    //console.log(contador)
+    if (contador == 0) {
+        console.log("O número atual é zero")
+    } else if (contador % 2 != 0) {
+        console.log("O número " + contador + " é Ímpar")
+    } else {
+        console.log(`O número ${contador} é Par`)
+    }
+}
+
+//while 
+let contador = 0;
+
+while (contador <= numeroDeAlunos) {
+    if (contador == 0) {
+        console.log("O número atual é zero")
+    } else if (contador % 2 == 0) {
+        console.log("O número " + contador + " é Par")
+    } else {
+        console.log(`O número ${contador} é Ímpar`)
+    }
+    contador++;
+}
+
+//for of
+let nomeDeAluno = ["Marcel", "Franklin", "Rita", "Afrânio"]
+
+for (let nome of nomeDeAluno) {
+    console.log(`Esta pessoa se chama ${nome}`)
+}
